d3e16eb19a089029bec2e123a086ced4fe1a03a2
Richard <richard.anaya@gmail.com>
2016-12-03 15:19:02 -0600

    flag system and completely custom scene

--- pages.js
+++ pages.js
@@ -3,24 +3,39 @@ function addPage(name,p){
     pages[window.location.pathname+name] = p;
 }
 addPage("",{
-  scene:"assets/howard_test_scene.dae",
-  sky:"assets/free.jpg",
+  scene:"assets/house.dae",
+  sky:"assets/house_sky.jpg",
+  description: "On a lonely autumn night, you are woken up from your sleep by strange lights coming through the window of your farmhouse. You look outside, and see a strange meteor falling through the sky, landing on a nearby hill. You get dressed and go outside to investigate...",
+  sound:"assets/sounds/house.mp3",
+  flags:["fog"],
   actions:[
     {
-      trigger: "pasted__pasted__pCube1",
-      text:"Go to test",
-      page:"test"
+      trigger: "hill",
+      description: "Climb the hill",
+      page:"climb_hill",
+      sound:"assets/sounds/hill.mp3",
+    },
+    {
+      trigger: "meteor",
+      description: "Approach the meteor",
+      page:"approach_meteor",
+      sound:"assets/sounds/meteor.mp3",
+    },
+    {
+      trigger: "chicken",
+      text: "The chicken was a sleep and did not like being rudely awoken.",
+      playSound:"assets/sounds/chicken.mp3"
     }
   ]
 })
-addPage("test",{
-  scene:"assets/scene_a.dae",
-  sky:"assets/free2.jpg",
+addPage("approach_meteor",{
+  customScene: "custom-scene-example"
+})
+addPage("climb_hill",{
+  scene:"assets/house.dae",
+  sky:"assets/house_sky.jpg",
+  description: "As you begin climbing the hill you hear the sound of engines approaching rapidly from the distance. Startled, you crouch down behind a log and watch as three helicopters descend to the same spot where you saw the meteor land.",
   actions:[
-    {
-      trigger:"Cat",
-      text:"Go to blah",
-      page:""
-    }
+      //TBD
   ]
 })
